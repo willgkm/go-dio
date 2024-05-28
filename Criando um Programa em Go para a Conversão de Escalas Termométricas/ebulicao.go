1d6cda1f9f1d0d17ca9deec179bd4e956817d4ed
willian.melo <wkuviatkovski@gmail.com>
2024-05-27 22:04:48 -0300

    criando repositorio com primeiro projeto

---
+++ Criando um Programa em Go para a Conversão de Escalas Termométricas/ebulicao.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	ebulicaoKelvin := 373.0
+	temperaturaCelcius := (ebulicaoKelvin - 273.0)
+	fmt.Printf("A temperatura de ebulição da água em Kelvin é %g e a temperatura em  é %g °C.", ebulicaoKelvin, temperaturaCelcius)
+
+}
